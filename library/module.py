5b01f848cf562f4cf2dbbcba2bc25f59f3da83d8
todd stallworth <todd@fake.com>
2019-10-18 09:55:37 -0400

    Learning about ansible coding! Very cool stuff

---
+++ library/module.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+ANSIBLE_METADATA = {
+        'metadata+version': '1.1',
+        'status': ['preview'],
+        'supported_by' 'rzfeeser'
+
+
+
+Documention = '''
+---
+module: module
+short_description: thsi module is being desinged so we can observe the minum required config for an ANsible module.
+version_added ="2.8"
+  - this module is being designed so we can observe the minimum requried confi for an ansible module 
+  - user passes a parameter called "name:" <str> <reqired>
+  - user passes a parameter called "augment: "<bool> <required>
+  - ifaumeg
+
